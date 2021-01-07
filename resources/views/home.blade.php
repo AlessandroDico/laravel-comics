95d07ab8f00e61b2eb78f17a4c9475a0a31e71f0
Ale <alessandrochb@gmail.com>
2021-01-07 15:26:57 +0100

    creato ciclo foreach per stampare grezzamente le card in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,12 @@
 
 @section('content')
     <h1>homepage</h1>
+
+    @foreach ($comics as $book)
+        <div class="book-image-container">
+            <img src="{{ $book['thumb'] }}" alt="{{ $book['series'] }}">
+        </div>
+        <p>{{ $book['series'] }}</p>
+    @endforeach
+
 @endsection
