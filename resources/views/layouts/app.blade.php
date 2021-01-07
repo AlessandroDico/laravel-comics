db6fe52d0fb5af66ce7b3515015d344e6ebf3d85
Ale <alessandrochb@gmail.com>
2021-01-07 16:13:12 +0100

    stilato header-top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        {{-- style --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        {{-- fontawesome --}}
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
         <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
     </head>
     <body>
         <header>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,14 @@
 <div class="header-top">
-    lorem
+    <div class="container">
+        <div class="header-top-links">
+            <a href="#" class="text-uppercase">DC Power℠ Visa®</a>
+            <a href="#" class="text-uppercase">Additional DC sites</a>
+            <i class="fas fa-chevron-down"></i>
+        </div>
+    </div>
 </div>
 <div class="header-bottom">
-    lorem
+    <div class="container">
+        lorem
+    </div>
 </div>
