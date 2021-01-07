244575b8cc2f7fb380c512a5c167573159f0b59a
Ale <alessandrochb@gmail.com>
2021-01-07 17:14:41 +0100

    stilato jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>homepage</h1>
-
-    @foreach ($comics as $book)
-        <div class="book-image-container">
-            <img src="{{ $book['thumb'] }}" alt="{{ $book['series'] }}">
+    @include('partials.jumbotron')
+    <div class="container">
+        <div class="cards-container">
+            @foreach ($comics as $book)
+                <div class="book-image-container">
+                    <img src="{{ $book['thumb'] }}" alt="{{ $book['series'] }}">
+                </div>
+                <p>{{ $book['series'] }}</p>
+            @endforeach
         </div>
-        <p>{{ $book['series'] }}</p>
-    @endforeach
+    </div>
 
 @endsection
