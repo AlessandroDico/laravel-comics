b2d6820f8f05c1fc94ffdab8270165e61625a1ac
Ale <alessandrochb@gmail.com>
2021-01-07 16:52:59 +0100

    stilato header bottom

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,57 @@
 <div class="header-top">
     <div class="container">
-        <div class="header-top-links">
+        <div class="header-top-container">
             <a href="#" class="text-uppercase">DC Power℠ Visa®</a>
             <a href="#" class="text-uppercase">Additional DC sites</a>
-            <i class="fas fa-chevron-down"></i>
+            <i class="fas fa-caret-down"></i>
         </div>
     </div>
 </div>
 <div class="header-bottom">
     <div class="container">
-        lorem
+        <div class="header-bottom-container">
+            <div class="header-logo-container">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="Logo DC comics">
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li>
+                        <a class="text-uppercase" href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Tv</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Games</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">News</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase" href="#">Shop</a>
+                        <i class="fas fa-caret-down"></i>
+                    </li>
+                </ul>
+            </div>
+            <div class="header-bottom-search">
+                <input type="text" placeholder="Search">
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
     </div>
 </div>
