7420200f696e220f51574be9e1a58a5da1ff57c5
Ale <alessandrochb@gmail.com>
2021-01-07 17:47:47 +0100

    aggiunto titolo e bottone alla sezione cards della home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     @include('partials.jumbotron')
     <section class="cards-section">
         <div class="container">
+            <h1 class="text-uppercase">current series</h1>
             <div class="cards-container">
                 @foreach ($comics as $book)
                     <div class="card">
@@ -13,6 +14,9 @@
                         <p class="text-uppercase">{{ $book['series'] }}</p>
                     </div>
                 @endforeach
+            </div>
+            <div class="cards-button">
+                <button type="button" class="text-uppercase">Load more</button>
             </div>
         </div>
     </section>
