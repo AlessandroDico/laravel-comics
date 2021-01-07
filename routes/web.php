dca81bc82c2fdfcf4e529be444895621d5bc5097
Ale <alessandrochb@gmail.com>
2021-01-07 15:26:30 +0100

    importati i dati nella Route tramite config( ) e passati alla view home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comicsArray = config('comicsData');
+
+    $data = [
+        'comics' => $comicsArray,
+    ];
+
+    return view('home', $data);
 });
