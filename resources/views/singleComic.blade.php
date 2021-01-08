6eec669963ff3f2fcc7735f45c2a3372f8056b29
Ale <alessandrochb@gmail.com>
2021-01-08 15:03:23 +0100

    creata riga post jumbotron in pag singolo fumetto con img fumetto

--- resources/views/singleComic.blade.php
+++ resources/views/singleComic.blade.php
@@ -3,6 +3,13 @@
 @section('content')
     @include('partials.jumbotron')
     <section class="single-comic">
+        <div class="post-card-row">
+            <div class="small-container">
+                <div class="post-card-image-container">
+                    <img src="{{ $book['thumb'] }}" alt="">
+                </div>
+            </div>
+        </div>
         <div class="small-container">
             <p>{{ $book['title'] }}</p>
         </div>
