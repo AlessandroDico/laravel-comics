fe28955021c5e958b5cbae3416c3721caba3db5b
Ale <alessandrochb@gmail.com>
2021-01-10 14:34:31 +0100

    creata e stilata sezione informazioni della pag singolo fumetto

--- resources/views/singleComic.blade.php
+++ resources/views/singleComic.blade.php
@@ -10,8 +10,38 @@
                 </div>
             </div>
         </div>
-        <div class="small-container">
-            <p>{{ $book['title'] }}</p>
+        <div class="comic-description">
+            <div class="small-container">
+                <div class="comic-description-container">
+                    <div class="comic-description-left">
+                        <h1>{{ $book['title'] }}</h1>
+                        <div class="comic-description-shop-info">
+                            <div class="shop-info-top">
+                                <span class="price">
+                                    <span>U.S. Price</span>
+                                    <span>{{ $book['price'] }}</span>
+                                </span>
+                                <span class="available text-uppercase">
+                                    Available
+                                </span>
+                                <span class="check-availability">
+                                    Check-Availability
+                                </span>
+                            </div><div class="shop-info-bottom">
+                                <p class="comic-description-paragraph">
+                                    {{ $book['description'] }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="comic-description-right">
+                        <span class="text-uppercase">Advertisement</span>
+                        <div class="description-img-container">
+                            <img src="{{ asset('images/adv.jpg') }}" alt="advertising image">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
